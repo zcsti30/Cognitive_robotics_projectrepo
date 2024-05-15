e485a5daf17ef5f58083fb425ee5f98617345ee2
zcsti30 <zcsti30@gmail.com>
2024-05-15 20:54:42 +0200

    Robot pozícióját sikeresen rajzolja térképen
    Van 1 subsciber az odometria adatokhoz.

--- cognitive_homework/scripts/test_marker_brokenline.py
+++ cognitive_homework/scripts/test_marker_brokenline.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-import math
 import rospy
-import numpy
 from visualization_msgs.msg import MarkerArray # Library for MarkerArray in Rviz
 from visualization_msgs.msg import Marker # Library for Marker in Rviz
+from nav_msgs.msg import Odometry
 
 rospy.init_node('broken_line_node')    # Init the node with a name
 
@@ -14,7 +13,9 @@ rospy.loginfo("broken_line_node Python node has started and publishing data on b
 
 rate = rospy.Rate(10)        # Set the Hz of the operation of the node 
 
-count = 0                   # Number of markers
+count = 0                    # Count the number of markers
+
+memorytime = 15              # Markers will be deleted after this time [sec]
 
 markerArray = MarkerArray()
 
@@ -31,49 +32,55 @@ deletemarker.action = deletemarker.DELETEALL
 deletemarker.id = 0
 deleteArray.markers.append(deletemarker)
 
-# For testing
-increment=0
+# We store the latest odometry data in this variable
+odom_data = Odometry()
+
+# Callback function and subscribe for current odometry data
+def odom_callback(msg):
+    global odom_data
+    odom_data = msg
+    
+rospy.Subscriber("/odom", Odometry, odom_callback, queue_size=1)
 
 # Run the node until Ctrl-C is pressed
 while not rospy.is_shutdown():  
    # Delete previous markers on the map
    marker_pub.publish(deleteArray)
 
-   # Creating new marker 
+   # Create new marker 
    marker = Marker()
    marker.header.frame_id = "odom"
    marker.type = marker.SPHERE
    marker.action = marker.ADD
    marker.header.stamp = rospy.Time.now()
-   marker.scale.x = 0.2
-   marker.scale.y = 0.2
-   marker.scale.z = 0.2
+   marker.scale.x = 0.05
+   marker.scale.y = 0.05
+   marker.scale.z = 0.05
    marker.color.a = 1.0
    marker.color.r = 1.0
    marker.color.g = 0.0
    marker.color.b = 0.0
    marker.pose.orientation.w = 1.0
-   # For testing
-   increment += 1
-   marker.pose.position.x = increment
-   marker.pose.position.y = increment
+   marker.pose.position.x = odom_data.pose.pose.position.x
+   marker.pose.position.y = odom_data.pose.pose.position.y
    marker.pose.position.z = 0 
 
    # Insert new marker
-   markerArray.markers.append(marker)
-   count += 1
+   if(marker.pose.position.x != 0):
+         markerArray.markers.append(marker)
+         count += 1
 
    # Remove oldest marker when reaching timeout
    if(count != 0):    
       currenttime = rospy.Time.now()
       markertime = markerArray.markers[0].header.stamp
 
-      if(currenttime.to_sec() - markertime.to_sec() > 4):
+      if(currenttime.to_sec() - markertime.to_sec() > memorytime):
          markerArray.markers.pop(0)
          count -= 1
 
    # Renumber the marker IDs
-   if(count != 0):     
+   if(count != 0):
       id = 0
       for m in markerArray.markers:
          m.id = id
@@ -81,6 +88,6 @@ while not rospy.is_shutdown():
 
       # Publish the array     
       marker_pub.publish(markerArray)
-   
+
    # Wait until next iteration
    rate.sleep()                
